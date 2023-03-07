2ac25d1271185fc4f128134f79797dea5d050f55
ADiestro <aldenjohn.diestro.cics@ust.edu.ph>
2023-03-07 23:57:06 +0800

    Added new formative activity

---
+++ FA1 - OOP Concepts in PHP (INDIVIDUAL)/frontpage.php
@@ -0,0 +1,27 @@
+<?php session_start();
+			
+				
+	?>	
+	
+
+<!DOCTYPE html>
+<html>
+	<head>
+
+		<meta charset="UTF-8">
+		
+		<title>FA1 - OOP Concepts in PHP (INDIVIDUAL)</title>
+
+		<link rel="stylesheet" href="css/style.css">
+
+	</head>
+	
+	<body>
+
+		test
+
+	</body>
+
+</html>
+
+
